acb1398c5175e9ef280ae8020cccc81e7196b561
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-08 23:01:41 +0530

    Initial commit

---
+++ Math/Q 1,2.py
@@ -0,0 +1,9 @@
+# 1.Write a Python program to convert degree to radian.
+# 2.Write a Python program to convert radian to degree.
+import math
+
+degree=float(input("Enter angle in degree : "))
+print("In Radian it is : {}".format(math.radians(degree)))
+
+radian=float(input("Enter angle in radain : "))
+print("In degree it is : {}".format(math.degrees(radian)))
