e14725ab55ec91e218e5fd22bb0fc064d47265ba
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-11 23:47:33 +0530

    Initial commit

--- Basic Questions Set - 1/Q 6, 8.py
+++ Basic Questions Set - 1/Q 6, 8.py
@@ -2,6 +2,13 @@
 # comma-separated numbers from user and generate a list
 # and a tuple with those numbers.
 
+# 8. Write a Python program to display the first and
+# last colors from the following list.
+
 a=input("Enter comma-separated numbers : ").split(',')
 print("List : {}".format(list(a)))
 print("Tuple : {}".format(tuple(a)))
+
+b=input("Enter list of colors : ").split()
+print("First color : {}".format(b[0]))
+print("Last color : {}".format(b[-1]))
