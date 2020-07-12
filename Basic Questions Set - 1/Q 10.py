9fd92f24b8b997aab9cf167a73d12204de0914bc
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-12 23:22:55 +0530

    Initial commit

---
+++ Basic Questions Set - 1/Q 10.py
@@ -0,0 +1,5 @@
+# 10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+# 16. Write a Python program to get the difference between a given number and 17,
+#     if the number is greater than 17 return double the absolute difference. 
+n=input("Enter a single digit number n : ")
+print(int(n)+int(n+n)+int(n+n+n))
