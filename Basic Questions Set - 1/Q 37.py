b2ad29acaf484e8968bee067daa456ff7f5a6a75
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-14 22:46:21 +0530

    Initial commit

---
+++ Basic Questions Set - 1/Q 37.py
@@ -0,0 +1,8 @@
+#  Write a Python program to display your details like name, age, address in three different lines.
+
+name = input()
+age = int(input())
+address = input()
+print('Name:',name)
+print('Age:',age)
+print('Address:',address)
