e4d92b177403bb848f88da96f3f6e74b5c33009c
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-20 22:41:57 +0530

    Initial commit

--- Basic Questions Set - 1/Q 40.py
+++ Basic Questions Set - 1/Q 40.py
@@ -1,6 +1,6 @@
 # 40. Write a Python program to compute the distance between the points (x1, y1) and (x2, y2).
 
-a,b=map(int,input("Enter first coordinates : ").split(','))
-c,d=map(int,input("Enter second coordinates : ").split())
+a,b=map(float,input("Enter first coordinates : ").split(','))
+c,d=map(float,input("Enter second coordinates : ").split(','))
 d=((a-c)**2+(b-d)**2)**(0.5)
 print("Distance : {}".format(d))
