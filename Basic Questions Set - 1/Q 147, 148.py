2a9e914ff460d9c131057241dd6c5b1d80ae1cfe
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-22 23:24:15 +0530

    Initial commit

---
+++ Basic Questions Set - 1/Q 147, 148.py
@@ -0,0 +1,3 @@
+# 147. Write a Python function to check whether a number is divisible by another number. Accept two integers values form the user.
+# 148. Write a Python function to find the maximum and minimum numbers from a sequence of numbers. Go to the editor
+#      Note: Do not use built-in functions.
