e26bc7ca50b74e1377b2c68f2ccd3fde88e068d3
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-22 23:33:36 +0530

    Initial commit

--- Basic Questions Set - 1/Q 147, 148.py
+++ Basic Questions Set - 1/Q 147, 148.py
@@ -1,3 +1,12 @@
-# 147. Write a Python function to check whether a number is divisible by another number. Accept two integers values form the user.
-# 148. Write a Python function to find the maximum and minimum numbers from a sequence of numbers. Go to the editor
+# 147. Write a Python function to check whether a number is divisible by another number.
+#      Accept two integers values form the user.
+# 148. Write a Python function to find the maximum and minimum numbers from a sequence of numbers.
 #      Note: Do not use built-in functions.
+
+print("To check if one number is divisible by another number")
+x=int(input("Enter number to be checked : "))
+y=int(input("Give the other number : "))
+if x%y==0:
+    print("{} is divisible by {}".format(x,y))
+else:
+    print("Not divisible")
