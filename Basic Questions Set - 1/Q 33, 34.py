8f286650ae1c0f3ed7511ef7ed617c816a9f1df0
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-25 23:36:13 +0530

    Initial commit

--- Basic Questions Set - 1/Q 33, 34.py
+++ Basic Questions Set - 1/Q 33, 34.py
@@ -3,7 +3,6 @@
 # 34. Write a Python program to sum of two given integers.
 # However, if the sum is between 15 to 20 it will return 20.
 
-# 33.
 x,y,z=map(int,input("Enter three numbers : ").split())
 if x==y or y==z or z==x:
     print("Two numbers are equal, so, result will be zero ! ")
